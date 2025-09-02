55a7d50da3902c7ec06148bf9f795a7fb4157958
maksym-t-dev <tropnikovmaksym08@gmail.com>
2025-09-02 14:36:54 +0200

    adding comments to a task 2

--- c_basics_hw_2/task_2.c
+++ c_basics_hw_2/task_2.c
@@ -5,36 +5,41 @@
 // ==========================================================
 #include <stdio.h>
 
+// Input prime number
 void input(int *n)
 {
     printf("Enter a number: ");
     scanf(" %d", n);
 }
 
-int checking_prime_numbers(int n, int divider)
+// Checking prime numbers
+int check_prime_nums(int n, int divider)
 {
-    if (n <= 1)
+    if (n <= 1) // n not less than 2
         return 0;
-    if (divider * divider > n)
+    if (divider * divider > n) // the square of the divisor is not greater than n
         return 1;
-    if (n % divider == 0)
+    if (n % divider == 0) // If n can be divided by another number, then it is false
         return 0;
 
-    return checking_prime_numbers(n, divider + 1);
+    return check_prime_nums(n, divider + 1); // increase the divisor
 }
 
-void print_prime_numbers(int n)
+// Print prime number
+void print_prime_num(int n)
 {
-    if (checking_prime_numbers(n, 2))
+    // If true
+    if (check_prime_nums(n, 2))
         printf("%d is prime\n", n);
+    // If false and other cases
     else
         printf("%d is not prime\n", n);
 }
 
 int main()
 {
-    int n;
+    int n; // prime number
 
     input(&n);
-    print_prime_numbers(n);
+    print_prime_num(n);
 }
